c657e3a1ea11533543049663733543672ef3e2d3
Can Bilgin <can.bilgin@authoritypartners.com>
2020-09-14 10:59:50 +0200

    chapter 2 and 3 changes

---
+++ chapter02/src/calculator.ios/ViewController.designer.cs
@@ -0,0 +1,164 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace calculator.ios
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton _btnNumber0 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton _btnNumber1 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton _btnNumber2 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton _btnNumber3 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton _btnNumber4 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton _btnNumber5 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton _btnNumber6 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton _btnNumber7 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton _btnNumber8 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton _btnNumber9 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton _btnOpAdd { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton _btnOpDivide { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton _btnOpEqual { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton _btnOpMultiply { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton _btnOpSubstract { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel _txtResult { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (_btnNumber0 != null) {
+                _btnNumber0.Dispose ();
+                _btnNumber0 = null;
+            }
+
+            if (_btnNumber1 != null) {
+                _btnNumber1.Dispose ();
+                _btnNumber1 = null;
+            }
+
+            if (_btnNumber2 != null) {
+                _btnNumber2.Dispose ();
+                _btnNumber2 = null;
+            }
+
+            if (_btnNumber3 != null) {
+                _btnNumber3.Dispose ();
+                _btnNumber3 = null;
+            }
+
+            if (_btnNumber4 != null) {
+                _btnNumber4.Dispose ();
+                _btnNumber4 = null;
+            }
+
+            if (_btnNumber5 != null) {
+                _btnNumber5.Dispose ();
+                _btnNumber5 = null;
+            }
+
+            if (_btnNumber6 != null) {
+                _btnNumber6.Dispose ();
+                _btnNumber6 = null;
+            }
+
+            if (_btnNumber7 != null) {
+                _btnNumber7.Dispose ();
+                _btnNumber7 = null;
+            }
+
+            if (_btnNumber8 != null) {
+                _btnNumber8.Dispose ();
+                _btnNumber8 = null;
+            }
+
+            if (_btnNumber9 != null) {
+                _btnNumber9.Dispose ();
+                _btnNumber9 = null;
+            }
+
+            if (_btnOpAdd != null) {
+                _btnOpAdd.Dispose ();
+                _btnOpAdd = null;
+            }
+
+            if (_btnOpDivide != null) {
+                _btnOpDivide.Dispose ();
+                _btnOpDivide = null;
+            }
+
+            if (_btnOpEqual != null) {
+                _btnOpEqual.Dispose ();
+                _btnOpEqual = null;
+            }
+
+            if (_btnOpMultiply != null) {
+                _btnOpMultiply.Dispose ();
+                _btnOpMultiply = null;
+            }
+
+            if (_btnOpSubstract != null) {
+                _btnOpSubstract.Dispose ();
+                _btnOpSubstract = null;
+            }
+
+            if (_txtResult != null) {
+                _txtResult.Dispose ();
+                _txtResult = null;
+            }
+        }
+    }
+}
