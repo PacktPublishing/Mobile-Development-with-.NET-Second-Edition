0aacaef9a0062b9fb79d31a38e9dd36ea63c559c
can.bilgin <can_bilgin@hotmail.com>
2020-10-25 09:40:50 +0100

    Chapter 4 resources

---
+++ chapter04/ShopAcross.Mobile/ShopAcross.Mobile.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ShopAcross.Mobile.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ chapter04/ShopAcross.Mobile/ShopAcross.Mobile.Core/LoginViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ShopAcross.Mobile.Core
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private string _userName = "Test";
+
+        private string _password;
+
+        private string _result;
+
+        private Command _loginCommand;
+
+        public LoginViewModel()
+        {
+            _loginCommand = new Command(Login, Validate);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                if (_userName != value)
+                {
+                    _userName = value;
+                    _loginCommand.ChangeCanExecute();
+                }
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if (_password != value)
+                {
+                    _password = value;
+                    _loginCommand.ChangeCanExecute();
+                }
+            }
+        }
+
+        public string Result
+        {
+            get { return _result; }
+            set
+            {
+                if (_result != value)
+                {
+                    _result = value;
+                    PropertyChanged?.Invoke(this, new
+                    PropertyChangedEventArgs(nameof(Result)));
+                }
+
+            }
+        }
+
+        public ICommand LoginCommand { get { return _loginCommand; } }
+
+        public void Login()
+        {
+            //TODO: Login
+            Result = "Successfully Logged In!";
+        }
+
+        public bool Validate()
+        {
+            return !string.IsNullOrEmpty(UserName) && !string.IsNullOrEmpty(Password);
+        }
+    }
+}
