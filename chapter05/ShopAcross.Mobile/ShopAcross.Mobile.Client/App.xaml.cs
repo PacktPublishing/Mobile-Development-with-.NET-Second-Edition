dce9e4c996181779f709891340e7db39f82f3ee5
can.bilgin <can_bilgin@hotmail.com>
2020-11-16 17:38:28 +0100

    initial checkin

---
+++ chapter05/ShopAcross.Mobile/ShopAcross.Mobile.Client/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShopAcross.Mobile.Client
+{
+    public partial class App : Application
+    {
+public App()
+{
+    InitializeComponent();
+    MainPage = new NavigationPage(new HomeView());
+}
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
