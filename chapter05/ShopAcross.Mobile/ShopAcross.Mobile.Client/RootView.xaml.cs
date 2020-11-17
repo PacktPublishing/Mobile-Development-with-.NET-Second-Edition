feef78da6fc29606479b73dee801028e87b565d8
can.bilgin <can_bilgin@hotmail.com>
2020-11-17 14:58:34 +0100

    Chapter 5 continuation

---
+++ chapter05/ShopAcross.Mobile/ShopAcross.Mobile.Client/RootView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShopAcross.Mobile.Client
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RootView : MasterDetailPage
+    {
+        public RootView()
+        {
+            InitializeComponent();
+
+            var list = new List<NavigationItem>();
+            list.Add(new NavigationItem { Id = 0, Title = "Home", Icon = "xamarin.png" });
+            list.Add(new NavigationItem { Id = 1, Title = "Settings", Icon = "xamarin.png" });
+
+            BindingContext = list;
+        }
+    }
+
+public class NavigationItem
+{
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Icon { get; set; }
+}
+}
---
+++ chapter05/ShopAcross.Mobile/ShopAcross.Mobile.Client/SettingsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ShopAcross.Mobile.Client
+{
+    public partial class SettingsView : ContentPage
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
